09e4b5053d02f1f43d3d4e7eaa0e3e801dfd7eab
PByzdra <yngwie@o2.pl>
2017-07-23 18:56:16 +0200

    Exercise 7.2. Forum User Immutable #1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -23,7 +24,10 @@ public class StreamMain {
         poemBeautifier.beautify("This is sample text", verse -> verse.toUpperCase());
         poemBeautifier.beautify("This is sample text", verse -> ("*** " + verse + " ***"));
         poemBeautifier.beautify("This is sample text", verse -> verse.replace('s', 'S'));
-        poemBeautifier.beautify("This is sample text", verse -> verse.substring(8));
+        poemBeautifier.beautify("This is sample text", verse -> verse.substring(8));//
+//Stream iterator
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 
